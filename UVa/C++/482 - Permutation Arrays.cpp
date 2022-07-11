4f1f759b20229708788def1212ab4e57b63caaa0
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2022-07-11 14:51:09 +0600

    Update 482 - Permutation Arrays.cpp

--- UVa/C++/482 - Permutation Arrays.cpp
+++ UVa/C++/482 - Permutation Arrays.cpp
@@ -1,4 +1,57 @@
-//Brute force implementation. need to learn a better algorithm
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+
+    cin>>t;
+    cin.ignore();
+
+    while(t--)
+    {
+        cin.ignore();
+        string a,b;
+        stringstream a1,b1;
+        getline(cin,a);
+        getline(cin,b);
+
+        a1<<a;
+        vector <int> v1;
+        int x;
+        while(a1>>x)
+            v1.push_back(x);
+
+        b1<<b;
+        vector <string> v2;
+        string temp;
+        while(b1>>temp)
+            v2.push_back(temp);
+
+        map <int, string> m;
+
+        for(int i=0;i <v1.size();i++)
+        {
+            m[v1[i]]= v2[i];
+        }
+        for(auto i: m)
+        {
+            cout<<(i.second)<<endl;
+        }
+
+        if(t)
+            cout<<endl;
+    }
+
+
+
+    return 0;
+}
+
+
+/*
 
 #include <bits/stdc++.h>
 
@@ -75,7 +128,7 @@ int main()
 
 
     return 0;
-}
+}*/
 
 
 
