aefdee7ed59efe28f259ce6a4c950efd40ebcc18
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2023-11-08 16:53:32 +0600

    Update comparators.cpp

--- comparators.cpp
+++ comparators.cpp
@@ -1,10 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct comp {
     bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
         return a.second > b.second; 
     }
 };
 
-auto cmp = [](int a, int b) { return a < b; };
+struct edge {
+    int u, v, w;
+    bool operator<(const edge& e) const {
+        return w > e.w;
+    }
+};
+
+struct node {
+    int x;
+    bool operator<(const node& n) const {
+        return x > n.x;
+    }
+};
 
 int main() {
     priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
@@ -13,14 +28,21 @@ int main() {
     while (!pq.empty()) {
         cout << pq.top().first << " " << pq.top().second << endl;
         pq.pop();
+    }   
+ 
+
+    set <edge > st;
+    st.insert({1, 2, 3});
+    st.insert({2, 3, 4});
+    for (auto& x : st) {
+        cout << x.u << " " << x.v << " " << x.w << endl;
     }
     
-    set<int, decltype(cmp)> s;
-    s.insert(1);
-    s.insert(2);
-
-    for (auto& x : s) {
-        cout << x << endl;
+    set <node> st2;
+    st2.insert({1});
+    st2.insert({2});
+    for (auto& x : st2) {
+        cout << x.x << endl;
     }
 
     return 0;
